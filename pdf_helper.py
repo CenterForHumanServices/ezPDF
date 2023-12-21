4d16d3504485a8747b2848c6c5693dfa1f65a8e1
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2023-12-21 14:37:36 -0800

    Create skeleton functions

---
+++ pdf_helper.py
@@ -0,0 +1,92 @@
+import fpdf2
+
+def create_pdf():
+    """_summary_
+    """
+    pass
+
+def add_page(pdf):
+    """_summary_
+
+    Args:
+        pdf (_type_): _description_
+    """
+    pass
+
+def add_empty_line(pdf, height= 20):
+    """_summary_
+
+    Args:
+        pdf (_type_): _description_
+        height (int): _description_
+    """
+    pass
+
+def add_one_cell_row(pdf, text, align="C"):
+    """_summary_
+
+    Args:
+        pdf (_type_): _description_
+        text (_type_): _description_
+        align (str): _description_
+    """
+    pass
+
+def add_two_cell_row(pdf, cell1, cell2, align="C"):
+    """_summary_
+
+    Args:
+        pdf (_type_): _description_
+        cell1 (_type_): _description_
+        cell2 (_type_): _description_
+        align (str): _description_
+    """
+    pass
+
+def add_three_cell_row(pdf, cell1, cell2, cell3, align="C"):
+    """_summary_
+
+    Args:
+        pdf (_type_): _description_
+        cell1 (_type_): _description_
+        cell2 (_type_): _description_
+        cell3 (_type_): _description_
+        align (str): _description_
+    """
+    pass
+
+def add_four_cell_row(pdf, cell1, cell2, cell3, cell4, align="C"):
+    """_summary_
+
+    Args:
+        pdf (_type_): _description_
+        cell1 (_type_): _description_
+        cell2 (_type_): _description_
+        cell3 (_type_): _description_
+        cell4 (_type_): _description_
+        align (str, optional): _description_. Defaults to "C".
+    """
+    pass
+
+def add_five_cell_row(pdf, cell1, cell2, cell3, cell4, cell5, align="C"):
+    """_summary_
+
+    Args:
+        pdf (_type_): _description_
+        cell1 (_type_): _description_
+        cell2 (_type_): _description_
+        cell3 (_type_): _description_
+        cell4 (_type_): _description_
+        cell5 (_type_): _description_
+        align (str, optional): _description_. Defaults to "C".
+    """
+    pass
+
+def export_pdf(pdf, filename):
+    """_summary_
+
+    Args:
+        pdf (_type_): _description_
+        filename (_type_): _description_
+    """
+    pass
